bed3ac6a0106c81b22887c5d5f3bed236663fe16
Daniel Estrada <destrada.work@gmail.com>
2024-05-01 14:33:07 +0200

    delete and update  apis

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -56,3 +56,57 @@ export const get_notes: RequestHandler = (async (req, res, next) => {
         next(error);
     }
   };
+
+interface update_note_params {
+	note_id: string
+}
+
+interface update_note_body {
+	title?: string,
+	text?: string,
+}
+
+  export const update_note: RequestHandler<update_note_params, unknown, update_note_body, unknown> = async(req, res, next) => {
+	const note_id = req.params.note_id;
+	const new_title = req.body.title;
+	const new_text = req.body.text;
+	try {
+		if(!mongoose.isValidObjectId(note_id))
+			throw createHttpError(400, "Invalid note id");
+		if (!new_title) 
+			throw createHttpError(400, "Note must have a title");
+
+		const note = await NoteModel.findById(note_id).exec();
+		if (!note) 
+		  throw createHttpError(404, "Note not found");
+		note.title = new_title;
+		note.text = new_text;
+
+		const updated_note = await note.save();
+		res.status(200).json(updated_note);
+	}
+	catch (error)
+	{
+		next(error);
+	}
+  };
+
+  export const delete_note: RequestHandler = async(req, res, next) => {
+	const note_id = req.params.note_id;
+    try {
+        if (!mongoose.isValidObjectId(note_id)) {
+            throw createHttpError(400, "Invalid note id");
+        }
+
+        const note = await NoteModel.findById(note_id).exec();
+        if (!note) {
+            throw createHttpError(404, "Note not found");
+        }
+
+        await note.deleteOne();
+		res.sendStatus(204);
+	}
+	catch (error) {
+		next(error);
+	}
+  };
--- backend/src/routes/notes.ts
+++ backend/src/routes/notes.ts
@@ -6,4 +6,6 @@ const router = express.Router();
 router.get("/", NotesController.get_notes);
 router.get("/:note_id", NotesController.get_note);
 router.post("/", NotesController.create_note);
+router.patch("/:note_id", NotesController.update_note);
+router.delete("/:note_id", NotesController.delete_note);
 export default router;
